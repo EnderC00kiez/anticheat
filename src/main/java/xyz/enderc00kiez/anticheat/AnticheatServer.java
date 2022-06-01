7cc1d0a0ce6e9b37e7526a8f26d49ff8c5841cb9
EnderC00kiez <49785023+EnderC00kiez@users.noreply.github.com>
2022-06-01 12:46:51 -0400

    dedi server to server

--- src/main/java/xyz/enderc00kiez/anticheat/AnticheatServer.java
+++ src/main/java/xyz/enderc00kiez/anticheat/AnticheatServer.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.DedicatedServerModInitializer;
 
 public class AnticheatServer implements DedicatedServerModInitializer {
     @Override
-    public void onInitializeDedicatedServer() {
+    public void onInitializeServer() {
         System.out.println("Enabled CSAC (server-side). All clients are forced to use it when joining your server.");
     }
 }
